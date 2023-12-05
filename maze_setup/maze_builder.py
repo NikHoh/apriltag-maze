e88be096664019518e2cb22d25fd51be834ef468
nhohmann <nikolas.hohmann@tu-darmstadt.de>
2023-12-05 14:40:09 +0100

    add yaml generation capability

--- maze_setup/maze_builder.py
+++ maze_setup/maze_builder.py
@@ -1,12 +1,64 @@
 from maze import Maze
 from typing import Optional
 from typing import List
+import os
+from wall import Tag
+from wall import Orientation
 
 def new_line():
     print("")
 
 def border_line():
     print("-------------------------------------------------------")
+
+def read_yaml_file(path:str) -> str:
+    """Reads in text from a path"""
+    assert os.path.isfile(path), f"Given path {path} does not exist"
+    with open(path) as f:
+        s = f.read()
+    return s
+
+def save_yaml_file(string_to_save:str, path:str):
+    """Writes text to a path"""
+    assert not os.path.isfile(path), "Filename that you want to use to save already exits. Choose another one."
+    with open(path, 'w') as f:
+        f.write(string_to_save)
+
+def replace_string_in_string(complete_string:str, old_string:str, new_string:str) -> str:
+    """Replaces a text within a string"""
+    assert old_string in complete_string, f"Expected string {old_string} not found in complete string"
+    return complete_string.replace(old_string, new_string)
+
+def get_yaml_line_for(tag: Tag, last_tag=False) -> str:
+    if tag.size.x == 140:
+        size = "0.084"   # the yaml file takes the inner size of the tag in m
+    elif tag.size.x == 28:
+        size = "0.0168"  # the yaml file takes the inner size of the tag in m
+    else:
+        assert 1 == 0, f"Tag size {tag.size} not known."
+    id = tag.tag_id
+    x = tag.position.x/1000  # tag file takes position in m
+    y = tag.position.y/1000  # tag file takes position in m
+    z = tag.position.z/1000  # tag file takes position in m
+
+    if tag.orientation == Orientation.NORTH:
+        orientation_string = "qw: 0.5, qx: 0.5, qy: -0.5, qz: -0.5"
+    elif tag.orientation == Orientation.WEST:
+        orientation_string = "qw: 0.7071068, qx: 0.7071068, qy: 0, qz: 0"
+    elif tag.orientation == Orientation.SOUTH:
+        orientation_string = "qw: 0.5, qx: 0.5, qy: 0.5, qz: 0.5"
+    elif tag.orientation == Orientation.EAST:
+        orientation_string = "qw: 0, qx: 0, qy: 0.7071068, qz: 0.7071068"
+    else:
+        assert 1 == 0, "Unknown orientation"
+
+    tag_string = f"{{id: {id}, size: {size}, x: {x}, y: {y}, z: {z}, {orientation_string}}}"
+    if last_tag:
+        tag_string += "\n"
+    else:
+        tag_string += ",\n"
+
+    return tag_string
 
 def get_positive_interger_input(message:str) -> int:
     """Asks for a user input being a positive integer"""
@@ -81,7 +133,7 @@ class MazeBuilder(object):
         print("When I ask you for the ID of a wall, I only need the smallest (and thus even) ID on the wall.")
         border_line()
         print("Important: Looking from the bird's perspective on the maze, I assume that the tag with the smallest ID "
-              "on the wall either 'looks' up (in negative x direction) or 'looks' left (in negative y direction)")
+              "on the wall either 'looks' up (in negative x direction called NORTH) or 'looks' left (in negative y direction called WEST)")
         border_line()
         print("Let's start building your maze.")
         num_rows = get_positive_interger_input("How many rows (in x direction) does your maze have?")
@@ -138,9 +190,36 @@ class MazeBuilder(object):
         self.maze.rows_with_horizontal_walls = tag_ids_horizontal_walls
         self.maze.rows_with_vertical_walls = tag_ids_vertical_walls
 
-    def save_yaml(self):
-        pass
-        
+    def save_maze_as_yaml(self):
+        while True:
+            while True:
+                save_name = input("Type in name of YAML file (no spaces)")
+                if " " in save_name:
+                    print("No spaces in save_name allowed")
+                else:
+                    break
+            if not ".yaml" in save_name:
+                save_name += ".yaml"
+            save_path = os.path.join(os.getcwd(), "output", save_name)
+
+            if os.path.isfile(save_path):
+                print("Name does already exist. Please choose other name.")
+            else:
+                break
+
+        maze_yaml_text = ""
+
+        for wall_idx, wall in enumerate(self.maze.walls):
+            for tag_idx, tag in enumerate(wall.tags):
+                maze_yaml_text += get_yaml_line_for(tag, last_tag=(wall_idx==len(self.maze.walls)-1 and tag_idx == len(wall.tags)-1))
+
+        file_content = read_yaml_file(os.path.join(os.getcwd(), "do_not_touch", "empty_tags.yaml"))
+
+        file_content = replace_string_in_string(file_content, "add_tag_description_here", maze_yaml_text)
+
+        save_yaml_file(file_content, save_path)
+
+        print(f"Maze yaml file was saved to {save_path}.")
 
 
 
@@ -148,7 +227,7 @@ def main():
     maze_builder = MazeBuilder()
     maze_builder.init_maze()
     maze_builder.get_maze_parameters_from_user()
-    maze_builder.save_yaml()
+    maze_builder.save_maze_as_yaml()
     print("End of program. Thanks and see you soon.")
 
 
